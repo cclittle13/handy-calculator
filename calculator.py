115b0131638616b6b218707ea38138f6ceb77216
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 15:29:59 -0800

    Added + and -

---
+++ calculator.py
@@ -0,0 +1,34 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
+def evaluate_math_input():
+
+    print "Welcome to our calculator!"
+
+    user_math_input = raw_input("Type your math problem here:\n>> ")
+
+    tokens = user_math_input.split(" ")
+
+    math_symbol = tokens[0]
+    num1 = int(tokens[1])
+    num2 = int(tokens[2])
+
+    if math_symbol == "+":
+        final = add(num1,num2)
+        #print final
+
+    elif math_symbol == "-":
+        final = subtract(num1,num2)
+
+    print final 
+
+evaluate_math_input()        
+
